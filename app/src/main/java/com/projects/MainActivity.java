ad9ac1e7422da9442e342074a440ce701ab9097e
Stefanos Gregory <gregorystefanos@gmail.com>
2022-01-05 11:58:07 +0200

    create login/register

--- app/src/main/java/com/projects/MainActivity.java
+++ app/src/main/java/com/projects/MainActivity.java
@@ -1,14 +1,65 @@
 package com.projects;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    EditText email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Firebase instance
+        auth = FirebaseAuth.getInstance();
+        // Text Inputs email and password
+        email = findViewById(R.id.editTextTextEmailAddress);
+        password = findViewById(R.id.editTextTextPassword);
+    }
+
+    // Login method
+    public  void login(View view){
+        auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            showMessage("Success", "login successfully");
+                        }else {
+                            showMessage("Error", task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
+    }
+
+    // Register method
+    public  void register(View view){
+        auth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            showMessage("Success", "register successfully uid: " + auth.getUid());
+                        }else {
+                            showMessage("Error", task.getException().getLocalizedMessage());
+                        }
+                    }
+                });
+        //auth.signOut();
+    }
+
+    void showMessage(String title, String message){
+        new AlertDialog.Builder(this).setTitle(title).setMessage(message).setCancelable(true).show();
     }
 }
